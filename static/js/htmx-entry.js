9b80c5d51ff85e516d2ef0bf014fe89db065f3cf
zachczx <zixian.chen@gmail.com>
2024-12-05 23:01:34 +0800

    Update htmx-entry.js

--- static/js/htmx-entry.js
+++ static/js/htmx-entry.js
@@ -1,3 +1,7 @@
 import htmx from 'htmx.org';
 
+// See https://github.com/bigskysoftware/htmx/issues/1690
+//
+// "At the moment you need some workarounds to use an extension with Vite, Rollup or other build tools because of a missing UMD wrapper.
+// Furthermore, the extensions are not minified like htmx.js."
 window.htmx = htmx;
