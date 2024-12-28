f76dc51dbf7bbc2f5559888b16069a868b82f7a7
zachczx <zixian.chen@gmail.com>
2024-12-28 11:03:37 +0800

    Tidied UI, removed idiomorph

--- static/js/htmx-bundle.js
+++ static/js/htmx-bundle.js
@@ -2,4 +2,3 @@ import './htmx-entry';
 import 'htmx-ext-response-targets/response-targets.js';
 import 'htmx-ext-preload/preload.js';
 import 'htmx-ext-sse/sse.js';
-import 'idiomorph/dist/idiomorph-ext.min.js';
