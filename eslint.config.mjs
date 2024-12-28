1c38d2d04a8436fd605520c879074cc5b6c9f533
zachczx <zixian.chen@gmail.com>
2024-12-29 02:36:00 +0800

    Added eslint prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{ languageOptions: { globals: globals.browser } },
 	{ ignores: ['static/js/output/', 'static/js/ext/', '**/**.config.js'] },
-	pluginJs.configs.recommended,
+	js.configs.recommended,
+	eslintConfigPrettier,
 ];
