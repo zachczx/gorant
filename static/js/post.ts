a1e20b3f09de72e561427a4f9160db408dedd5fd
zachczx <zixian.chen@gmail.com>
2025-01-25 09:50:16 +0800

    Update post.ts
    Cleanup

--- static/js/post.ts
+++ static/js/post.ts
@@ -1,39 +1,6 @@
 import tags from './tags';
 import { keyboardShortcut } from './common';
 
-/**
- * Post Description on Post page.
- * To show and hide edit post description form.
- */
-(function activateDescriptionForm() {
-	const postDescriptionStatic = document.getElementById('post-description-static') as HTMLButtonElement;
-	const postDescriptionForm = document.getElementById('post-description-form') as HTMLFormElement;
-	if (postDescriptionStatic) {
-		postDescriptionStatic.addEventListener('click', () => {
-			postDescriptionForm?.classList.remove('hidden');
-			postDescriptionStatic?.classList.add('hidden');
-		});
-	}
-
-	const postDescriptionCancel = document.getElementById('post-description-cancel') as HTMLButtonElement;
-	if (postDescriptionCancel) {
-		postDescriptionCancel.addEventListener('click', () => {
-			postDescriptionForm.classList.add('hidden');
-			postDescriptionStatic.classList.remove('hidden');
-		});
-	}
-
-	const moreActionsEditButton = document.getElementById('more-actions-edit-button') as
-		| HTMLButtonElement
-		| HTMLLabelElement;
-	if (moreActionsEditButton) {
-		moreActionsEditButton.addEventListener('click', () => {
-			postDescriptionForm?.classList.remove('hidden');
-			postDescriptionStatic?.classList.add('hidden');
-		});
-	}
-})();
-
 // Post title
 const postTitleTruncated = document.getElementById('post-title-truncated') as HTMLButtonElement;
 const postTitleTruncatedClasses = ['line-clamp-2', 'max-h-[6.3rem]'];
@@ -55,19 +22,6 @@ postTitleTruncated.addEventListener('click', (evt) => {
 		console.log('inside');
 		deletePostButton.addEventListener('click', () => {
 			deletePostModal.showModal();
-		});
-	}
-})();
-
-/**
- * Button to copy link to post.
- */
-(function copyPostLink() {
-	const moreActionsCopyButton = document.getElementById('more-actions-copy-button');
-	if (moreActionsCopyButton) {
-		// const postId = moreActionsCopyButton.dataset.postId.slice(5)
-		moreActionsCopyButton.addEventListener('click', () => {
-			navigator.clipboard.writeText(window.location.href);
 		});
 	}
 })();
